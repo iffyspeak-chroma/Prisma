9f3a5a85b034ded8ce98bf40bab053ff37a4ca67
Sonny <60908891+iffyspeak-chroma@users.noreply.github.com>
2025-11-21 15:28:17 -0600

    Created a place to store necessary server information. Instanced for good luck!

---
+++ Server/Server.cs
@@ -0,0 +1,12 @@
+﻿using Server.ConfigurationTools;
+using API.Networking;
+
+namespace Server;
+
+public class Server
+{
+    public static Server? Instance = null;
+
+    public ConfigMapping? Configuration = null;
+    public PacketReport? PacketReport = null;
+}
