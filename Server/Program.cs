3ae0b8b33a7a213306ca8246684689dcdb388b40
Sonny <60908891+iffyspeak-chroma@users.noreply.github.com>
2025-11-21 03:08:01 -0600

    Just a TODO so I can keep it up. Last commit of the night. I'm a sleepy guy.

--- Server/Program.cs
+++ Server/Program.cs
@@ -6,6 +6,8 @@ class Program
 {
     static void Main(string[] args)
     {
-        
+        // TODO: Load in Minecraft Server's packet report from the data generator, some settings for Prisma,
+        // and then start DotNetty
+        // @iffyspeak-chroma
     }
 }
