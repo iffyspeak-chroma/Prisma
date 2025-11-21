ac828abce5374e821a443e121600484b3c577c01
Sonny <60908891+iffyspeak-chroma@users.noreply.github.com>
2025-11-21 14:45:15 -0600

    Get started for precheck before actually launching the server.

---
+++ Server/Constants.cs
@@ -0,0 +1,19 @@
+﻿namespace Server;
+
+public class Constants
+{
+    #region Directories
+
+    public static string ExecutingDirectory = Environment.CurrentDirectory;
+    public static string ConfigurationDirectory = Path.Join(ExecutingDirectory, "config");
+
+    #endregion
+    
+    #region Files
+    
+    public static string ConfigurationFile = Path.Combine(ConfigurationDirectory, "config.json");
+    public static string PacketReportFile = Path.Combine(ConfigurationDirectory, "packets.json");
+    
+    #endregion
+
+}
--- Server/Program.cs
+++ Server/Program.cs
@@ -6,8 +6,51 @@ class Program
 {
     static void Main(string[] args)
     {
-        // TODO: Load in Minecraft Server's packet report from the data generator, some settings for Prisma,
-        // and then start DotNetty
-        // @iffyspeak-chroma
+        // Start by checking if the server has run before
+        if (FirstLaunch())
+        {
+            // So the server hasn't been run before. We should do the first setups before continuing.
+            Setup();
+        }
+
+        if (RequiredFileCheck() != 0)
+        {
+            Environment.Exit(RequiredFileCheck());
+        }
+    }
+    
+    static bool FirstLaunch()
+    {
+        if (Directory.Exists(Constants.ConfigurationDirectory))
+        {
+            // It likely has been launched before and should be okay to continue. Without any further setup
+            return false;
+        }
+
+        return true;
+    }
+
+    static void Setup()
+    {
+        try
+        {
+            // Create all necessary directories
+            // For now, this is technically the only one we'll need
+            Directory.CreateDirectory(Constants.ConfigurationDirectory);
+        }
+        finally
+        {
+            LogTool.Info("Because this is your first time running Prisma: you should configure your server then launch Prisma again.");
+            LogTool.Info("Exiting!");
+            Environment.Exit(0);
+        }
+    }
+
+    static int RequiredFileCheck()
+    {
+        int _configFile = 0b01;
+        int _packetsReport = 0b10;
+        
+        return (File.Exists(Constants.ConfigurationFile) ? 0 : _configFile) | (File.Exists(Constants.PacketReportFile) ? 0 : _packetsReport);
     }
 }
