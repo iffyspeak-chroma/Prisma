5dbe3cd38fb529f8c23c2ed3fb33ad1675bc1fca
Sonny <60908891+iffyspeak-chroma@users.noreply.github.com>
2025-11-22 17:43:59 -0600

    Temporary solution for testing login stuff

---
+++ Server/Packets/Login/Clientbound/ClientboundLoginDisconnectPacket.cs
@@ -0,0 +1,22 @@
+﻿using API.Networking;
+using DotNetty.Transport.Channels;
+using Server.Players;
+
+namespace Server.Packets.Login.Clientbound;
+
+public class ClientboundLoginDisconnectPacket : ICallable
+{
+    public async void Call(IChannelHandlerContext context, Packet? packet)
+    {
+        using (Packet p = new Packet(packet.ToArray()))
+        {
+            p.InsertInt(PacketReport.Mapping.Login.Clientbound["minecraft:login_disconnect"].Id);
+            p.WriteLength();
+
+            await PlayerManager.Instance.ConnectedClients[context.Channel].SendPacket(p);
+            
+            PlayerManager.Instance.ConnectedClients[context.Channel].DisconnectChannel();
+            PlayerManager.Instance.ConnectedClients.Remove(context.Channel);
+        }
+    }
+}
--- Server/Packets/Login/Serverbound/ServerboundLoginStartPacket.cs
+++ Server/Packets/Login/Serverbound/ServerboundLoginStartPacket.cs
@@ -1,6 +1,9 @@
 ﻿using API.DataTypes.Player;
+using API.Logging;
 using API.Networking;
+using API.TextComponents;
 using DotNetty.Transport.Channels;
+using Server.Packets.Login.Clientbound;
 using Server.Players;
 
 namespace Server.Packets.Login.Serverbound;
@@ -13,5 +16,21 @@ public class ServerboundLoginStartPacket : ICallable
         Guid playerId = packet.ReadGuid();
 
         PlayerManager.Instance.ConnectedClients[context.Channel].Player = new ServerPlayer(playerName, playerId);
+        
+        LogTool.Info($"{playerName}[{playerId.ToString()}] is attempting to connect to the server.");
+        
+        // TODO: Replace this with the actual next step.
+        using (Packet p = new Packet())
+        {
+            // I'm just going to disconnect them for now
+            TextComponentBuilder builder = new TextComponentBuilder();
+
+            builder.AddText("Uh oh!\n", color: "red");
+            builder.AddText("I'm not quite ready for that kinda thing just yet.");
+            
+            p.Write(builder.Build());
+
+            new ClientboundLoginDisconnectPacket().Call(context, p);
+        }
     }
 }
