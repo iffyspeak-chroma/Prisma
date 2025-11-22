684254a71bb3ea812937a790704b2cf3accbc9ca
Sonny <60908891+iffyspeak-chroma@users.noreply.github.com>
2025-11-21 20:08:34 -0600

    Simple little player manager for the time being.

---
+++ Server/Players/PlayerManager.cs
@@ -0,0 +1,10 @@
+﻿using API.DataTypes.Player;
+
+namespace Server.Players;
+
+public class PlayerManager
+{
+    public PlayerManager Instance = new  PlayerManager();
+    
+    public List<NetworkedClient> ConnectedClients = new List<NetworkedClient>();
+}
