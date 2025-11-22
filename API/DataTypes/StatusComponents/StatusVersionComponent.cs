c69feae9ece4817f17a6899ea428ecbc1576c9f2
Sonny <60908891+iffyspeak-chroma@users.noreply.github.com>
2025-11-21 22:58:15 -0600

    This is no longer ChromaServer's! HaHA!

--- API/DataTypes/StatusComponents/StatusVersionComponent.cs
+++ API/DataTypes/StatusComponents/StatusVersionComponent.cs
@@ -4,7 +4,7 @@ namespace API.DataTypes.StatusComponents;
 
 public class StatusVersionComponent
 {
-    [JsonPropertyName("name")] public string VersionName { get; set; } = "ChromaServer 1.21.9";
+    [JsonPropertyName("name")] public string VersionName { get; set; } = "Prisma 1.21.9";
 
     [JsonPropertyName("protocol")] public int VersionNumber { get; set; } = 773;
 }
