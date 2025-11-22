547849872c8f0ffa6ba5483fb6691909cbc09cd6
Sonny <60908891+iffyspeak-chroma@users.noreply.github.com>
2025-11-21 21:25:38 -0600

    Status Request/Response implementation

---
+++ Server/Packets/Status/Clientbound/StatusResponsePacket.cs
@@ -0,0 +1,21 @@
+﻿using API.Networking;
+using DotNetty.Transport.Channels;
+using Server.Players;
+
+namespace Server.Packets.Status.Clientbound;
+
+public class ClientboundStatusStatusResponse : ICallable
+{
+    public async void Call(IChannelHandlerContext context, Packet? packet)
+    {
+        //throw new NotImplementedException();
+
+        using (Packet p = new Packet(packet.ToArray()))
+        {
+            p.InsertInt(PacketReport.Mapping.Status.Clientbound["minecraft:status_response"].Id);
+            p.WriteLength();
+            
+            await PlayerManager.Instance.ConnectedClients[context.Channel].SendPacket(p);
+        }
+    }
+}
---
+++ Server/Packets/Status/Serverbound/StatusRequestPacket.cs
@@ -0,0 +1,34 @@
+﻿using System.Text.Json;
+using API.DataTypes;
+using API.Logging;
+using API.Networking;
+using API.TextComponents;
+using DotNetty.Transport.Channels;
+
+namespace Server.Packets.Status.Serverbound;
+
+public class StatusRequestPacket : ICallable
+{
+    public void Call(IChannelHandlerContext context, Packet? packet)
+    {
+        LogTool.Debug($"{context.Channel.RemoteAddress} sent a status request", Server.Instance.Configuration.DebugMode);
+
+        // TODO: We should only really be doing this if the server settings permit
+        
+        StatusResponseFormat status = new StatusResponseFormat();
+
+        TextComponentBuilder builder = new TextComponentBuilder();
+        builder.AddText("A Prisma Minecraft server", color: "green", bold: true);
+
+        status.Description = builder;
+        
+        string response = JsonSerializer.Serialize(status);
+
+        using (Packet p = new Packet())
+        {
+            p.Write(response);
+            
+            
+        }
+    }
+}
