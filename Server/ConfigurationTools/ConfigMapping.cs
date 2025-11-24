df64a1ff814b0bb355ad9a55a5d654c6ed391bc8
Sonny <60908891+iffyspeak-chroma@users.noreply.github.com>
2025-11-24 16:23:50 -0600

    Add configuration for encryption

--- Server/ConfigurationTools/ConfigMapping.cs
+++ Server/ConfigurationTools/ConfigMapping.cs
@@ -5,4 +5,5 @@ public class ConfigMapping
     public string BindAddress { get; set; } = "0.0.0.0";
     public ushort Port { get; set; } = 26656;
     public bool DebugMode { get; set; } = false;
+    public bool DoEncryption { get; set; } = true;
 }
