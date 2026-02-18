4b1020af42851bb05890d2c8a29c53f33d91ce4a
Sonny <60908891+iffyspeak-chroma@users.noreply.github.com>
2026-02-18 00:49:23 -0600

    Added a way to communicate plugin messages. I have no idea when it'll be used but I'm sure it will eventually.

---
+++ Server/Packets/Configuration/Clientbound/ClientboundConfigurationPluginMessagePacket.cs
@@ -0,0 +1,22 @@
+﻿using API.Networking;
+using DotNetty.Transport.Channels;
+using Server.Players;
+
+namespace Server.Packets.Configuration.Clientbound;
+
+public class ClientboundConfigurationPluginMessagePacket : ICallable
+{
+    public async void Call(IChannelHandlerContext context, Packet? packet)
+    {
+        using (Packet p = new Packet())
+        {
+            p.Write("minecraft:brand"); // Identifier
+            p.Write("Prisma"); // Data
+            
+            p.InsertInt(PacketReport.Mapping.Configuration.Clientbound["minecraft:custom_payload"].Id);
+            p.WriteLength();
+            
+            await PlayerManager.Instance.ConnectedClients[context.Channel].SendPacket(p);
+        }
+    }
+}
