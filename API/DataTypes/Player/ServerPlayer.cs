c85c46a8f870013ed3e108a0adf8fa435493f361
Sonny <60908891+iffyspeak-chroma@users.noreply.github.com>
2026-02-18 00:03:50 -0600

    Add a place to store retrievable plugin messages.

--- API/DataTypes/Player/ServerPlayer.cs
+++ API/DataTypes/Player/ServerPlayer.cs
@@ -5,7 +5,10 @@ public class ServerPlayer
     public string Username { get; set; } = "";
     public Guid Uuid { get; set; } = Guid.Empty;
     public ClientSettings? ClientSettings = null;
-    
+    public Dictionary<string, object?> PluginMessages { get; set; } = new Dictionary<string, object?>()
+    {
+        {"minecraft:brand", null}
+    };
     public ServerPlayer() {}
 
     public ServerPlayer(string u, Guid gu)
