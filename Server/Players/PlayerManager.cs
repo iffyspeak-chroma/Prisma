111d714f38caee9b08edb20494d8a0d0deccfa4e
Sonny <60908891+iffyspeak-chroma@users.noreply.github.com>
2026-02-17 23:59:07 -0600

    Add a place to retrieve a string for a specific player.

--- Server/Players/PlayerManager.cs
+++ Server/Players/PlayerManager.cs
@@ -7,6 +7,10 @@ namespace Server.Players;
 public class PlayerManager
 {
     public static readonly PlayerManager Instance = new  PlayerManager();
+    public static string GetPlayerIdentifier(ServerPlayer player)
+    {
+        return $"{player.Username}[{player.Uuid.ToString()}]";
+    }
     
     public Dictionary<IChannel, NetworkedClient> ConnectedClients = new Dictionary<IChannel, NetworkedClient>();
 }
