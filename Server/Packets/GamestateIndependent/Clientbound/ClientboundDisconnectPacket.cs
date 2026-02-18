3d28c82b9e2c848fb4dff51df7daf6fe75b4a5b9
Sonny <60908891+iffyspeak-chroma@users.noreply.github.com>
2026-02-18 01:20:01 -0600

    Add a way to disconnect players from the PlayerManager

--- Server/Packets/GamestateIndependent/Clientbound/ClientboundDisconnectPacket.cs
+++ Server/Packets/GamestateIndependent/Clientbound/ClientboundDisconnectPacket.cs
@@ -12,13 +12,6 @@ public class ClientboundDisconnectPacket : ICallable
     public async void Call(IChannelHandlerContext context, Packet? packet)
     {
         NetworkedClient client = PlayerManager.Instance.ConnectedClients[context.Channel];
-        
-        /*
-        TextComponentBuilder builder = new TextComponentBuilder();
-
-        builder.AddText("Uh oh!\n", color: "red");
-        builder.AddText("I'm not quite ready for that kinda thing just yet.");
-        */
             
         packet.Write(DisconnectMessage.Build());
 
@@ -50,5 +43,39 @@ public class ClientboundDisconnectPacket : ICallable
         
         PlayerManager.Instance.ConnectedClients[context.Channel].DisconnectChannel();
         PlayerManager.Instance.ConnectedClients.Remove(context.Channel);
+    }
+    
+    public async void Call(NetworkedClient client, Packet? packet)
+    {
+        packet.Write(DisconnectMessage.Build());
+
+        switch (client.Gamestate)
+        {
+            case PlayerGamestate.Login:
+            {
+                packet.InsertInt(PacketReport.Mapping.Login.Clientbound["minecraft:login_disconnect"].Id);
+                packet.WriteLength();
+                
+                await PlayerManager.Instance.ConnectedClients[client.Channel].SendPacket(packet);
+                break;
+            }
+
+            case PlayerGamestate.Configuration:
+            {
+                packet.InsertInt(PacketReport.Mapping.Configuration.Clientbound["minecraft:disconnect"].Id);
+                packet.WriteLength();
+                
+                await PlayerManager.Instance.ConnectedClients[client.Channel].SendPacket(packet);
+                break;
+            }
+
+            default:
+            {
+                throw new NotImplementedException();
+            }
+        }
+        
+        PlayerManager.Instance.ConnectedClients[client.Channel].DisconnectChannel();
+        PlayerManager.Instance.ConnectedClients.Remove(client.Channel);
     }
 }
--- Server/Players/PlayerManager.cs
+++ Server/Players/PlayerManager.cs
@@ -1,15 +1,33 @@
 ﻿using System.Diagnostics;
 using API.DataTypes.Player;
+using API.Networking;
+using API.TextComponents;
 using DotNetty.Transport.Channels;
+using Server.Packets.GamestateIndependent.Clientbound;
 
 namespace Server.Players;
 
 public class PlayerManager
 {
     public static readonly PlayerManager Instance = new  PlayerManager();
-    public static string GetPlayerIdentifier(ServerPlayer player)
+
+    public static void DisconnectPlayer(NetworkedClient player, TextComponentBuilder reason)
     {
-        return $"{player.Username}[{player.Uuid.ToString()}]";
+        ClientboundDisconnectPacket packet = new ClientboundDisconnectPacket();
+        packet.DisconnectMessage = reason;
+        
+        packet.Call(player, new Packet());
+    }
+    
+    public static void DisconnectPlayer(NetworkedClient player, string reason)
+    {
+
+        TextComponentBuilder builder = new TextComponentBuilder();
+
+        builder.AddText("Uh oh!\n", color: "red");
+        builder.AddText($"{reason}");
+
+        DisconnectPlayer(player, builder);
     }
     
     public Dictionary<IChannel, NetworkedClient> ConnectedClients = new Dictionary<IChannel, NetworkedClient>();
