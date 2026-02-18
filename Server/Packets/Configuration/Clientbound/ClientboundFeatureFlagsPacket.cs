991b686d2f3a1b277a5a1a4073828615c1557402
Sonny <60908891+iffyspeak-chroma@users.noreply.github.com>
2026-02-18 01:22:47 -0600

    Add the feature flags packet.

---
+++ Server/Packets/Configuration/Clientbound/ClientboundFeatureFlagsPacket.cs
@@ -0,0 +1,31 @@
+﻿using API.Networking;
+using DotNetty.Transport.Channels;
+using Server.Players;
+
+namespace Server.Packets.Configuration.Clientbound;
+
+public class ClientboundFeatureFlagsPacket : ICallable
+{
+    public async void Call(IChannelHandlerContext context, Packet? packet)
+    {
+        using (Packet p = new Packet())
+        {
+            List<string> featureIdentifiers = new List<string>()
+            {
+                "minecraft:vanilla"
+            };
+            
+            p.Write(featureIdentifiers.Count);
+            
+            foreach (string identifier in featureIdentifiers)
+            {
+                p.Write(identifier);
+            }
+            
+            p.InsertInt(PacketReport.Mapping.Configuration.Clientbound["minecraft:update_enabled_features"].Id);
+            p.WriteLength();
+            
+            await PlayerManager.Instance.ConnectedClients[context.Channel].SendPacket(p);
+        }
+    }
+}
