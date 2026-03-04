a7493406cc3d16640f350e551e90967b6efc9f03
Sonny <60908891+iffyspeak-chroma@users.noreply.github.com>
2026-03-03 18:00:28 -0600

    Create the Identifier DataType

---
+++ API/DataTypes/Mojang/Identifier.cs
@@ -0,0 +1,60 @@
+﻿using System.Text.RegularExpressions;
+
+namespace API.DataTypes.Mojang;
+
+public class Identifier
+{
+    public string Namespace { get; private set; }
+    public string Value { get; private set; }
+
+    public Identifier(string ns, string value)
+    {
+        ValidateNamespace(ns);
+        ValidateValue(value);
+        
+        this.Namespace = ns;
+        this.Value = value;
+    }
+
+    public Identifier(string value)
+    {
+        ValidateValue(value);
+        
+        this.Namespace = "minecraft";
+        this.Value = value;
+    }
+
+    public static Identifier Parse(string identifier)
+    {
+        string ns = "prisma";
+        string v = "null";
+        
+        string[] parts = identifier.Split(":");
+
+        if (parts.Length == 1)
+        {
+            ns = "minecraft";
+            v = parts[0];
+
+            return new Identifier(ns, v);
+        }
+
+        return new Identifier(parts[0], parts[1]);
+    }
+
+    private void ValidateNamespace(string ns)
+    {
+        if (!Regex.IsMatch(ns, "^[a-z0-9._-]+$"))
+        {
+            throw new ArgumentException("Namespace contains invalid characters!");
+        }
+    }
+
+    private void ValidateValue(string va)
+    {
+        if (!Regex.IsMatch(va, "^[a-z0-9._/-]+$"))
+        {
+            throw new ArgumentException("Value contains invalid characters!");
+        }
+    }
+}
