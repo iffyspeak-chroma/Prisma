9a84bef0c110c5c036e12c5f921fbfbc454fa621
Sonny <60908891+iffyspeak-chroma@users.noreply.github.com>
2026-03-03 20:08:34 -0600

    Extend out DataPackIdentity to use Identifier

--- API/DataTypes/DataPacks/DataPackIdentity.cs
+++ API/DataTypes/DataPacks/DataPackIdentity.cs
@@ -1,10 +1,24 @@
-﻿namespace API.DataTypes.DataPacks;
+﻿using API.DataTypes.Mojang;
+
+namespace API.DataTypes.DataPacks;
 
 public class DataPackIdentity
 {
     public string Namespace { get; set; }
     public string ID { get; set; }
     public string Version { get; set; }
-    
-    public DataPackIdentity() {}
+
+    public DataPackIdentity(string ns, string id, string version)
+    {
+        this.Namespace = ns;
+        this.ID = id;
+        this.Version = version;
+    }
+
+    public DataPackIdentity(Identifier identifier, string version)
+    {
+        this.Namespace = identifier.Namespace;
+        this.ID = identifier.Value;
+        this.Version = version;
+    }
 }
