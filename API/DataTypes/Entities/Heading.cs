3418c150c717635cb410dd3c95b3eebbec69902f
Sonny <60908891+iffyspeak-chroma@users.noreply.github.com>
2026-03-04 14:42:18 -0600

    Create heading class

---
+++ API/DataTypes/Entities/Heading.cs
@@ -0,0 +1,13 @@
+﻿namespace API.DataTypes.Entities;
+
+public class Heading
+{
+    public float Yaw;
+    public float Pitch;
+
+    public Heading()
+    {
+        Yaw = 0;
+        Pitch = 0;
+    }
+}
