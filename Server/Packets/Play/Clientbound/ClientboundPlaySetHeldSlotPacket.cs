11c61ad202f72d8f67fa89b4b0c91fa1d3b1ff74
Sonny <60908891+iffyspeak-chroma@users.noreply.github.com>
2026-03-04 12:27:23 -0600

    Add held slot packet

--- Server/Packets/Play/Clientbound/ClientboundPlaySetHeldSlotPacket.cs
+++ Server/Packets/Play/Clientbound/ClientboundPlaySetHeldSlotPacket.cs
@@ -5,29 +5,25 @@ using Server.Managers;
 
 namespace Server.Packets.Play.Clientbound;
 
-public class ClientboundPlayPlayerAbilitiesPacket : ICallable
+public class ClientboundPlaySetHeldSlotPacket : ICallable
 {
     public async void Call(IChannelHandlerContext context, Packet? packet)
     {
         NetworkedClient client = PlayerManager.Instance.ConnectedClients[context.Channel];
-        CheatAbilities abilities = CheatAbilities.Invulnerable | CheatAbilities.PermitFlying;
-
+        
         using (Packet p = new Packet())
         {
-            p.Write((byte) abilities);
-
-            // Flight speed
-            p.Write(0.05f);
+            // TODO: Proper implementation
+            // But for now we'll just use the date.
+            DateTime date = new DateTime();
+            int heldSlot = (date.Day % 8);
             
-            // FOV Mod
-            p.Write(0.1f);
+            p.Write(heldSlot);
             
-            p.InsertInt(PacketReport.Mapping.Play.Clientbound["minecraft:player_abilities"].Id);
+            p.InsertInt(PacketReport.Mapping.Play.Clientbound["minecraft:set_held_slot"].Id);
             p.WriteLength();
 
             await client.SendPacket(p);
-            
-            new ClientboundPlaySetHeldSlotPacket().Call(context, null);
         }
     }
 }
