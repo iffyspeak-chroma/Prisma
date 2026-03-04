c97391c29e3a9c59b2523d3e43d4c8c15656b475
Sonny <60908891+iffyspeak-chroma@users.noreply.github.com>
2026-03-04 14:31:13 -0600

    Add a class to track location

---
+++ API/DataTypes/Location.cs
@@ -0,0 +1,31 @@
+﻿using API.Networking;
+
+namespace API.DataTypes;
+
+public class Location : IWriteToPackets
+{
+    public double X { get; set; }
+    public double Y { get; set; }
+    public double Z { get; set; }
+
+    public Location()
+    {
+        X = 0d;
+        Y = 0d;
+        Z = 0d;
+    }
+
+    public Location(double x, double y, double z)
+    {
+        X = x;
+        Y = y;
+        Z = z;
+    }
+
+    public void Write(Packet packet)
+    {
+        packet.Write(X);
+        packet.Write(Y);
+        packet.Write(Z);
+    }
+}
