8388aae0f5688d9cfe2ee098186615233f5d3c78
Sonny <60908891+iffyspeak-chroma@users.noreply.github.com>
2026-03-03 18:09:30 -0600

    Lets try and determine the registry issue.

--- Server/Packets/Configuration/Serverbound/ServerboundConfigurationKnownPacksPacket.cs
+++ Server/Packets/Configuration/Serverbound/ServerboundConfigurationKnownPacksPacket.cs
@@ -28,6 +28,7 @@ public class ServerboundConfigurationKnownPacksPacket : ICallable
         
         LogTool.Info($"{NetworkedClient.GetPlayerIdentifier(client.Player)} selected {packs.Count} pack(s).");
         
-        new ClientboundConfigurationFinishPacket().Call(context, null); 
+        new ClientboundConfigurationRegistryDataPacket().Call(context, null);
+        //new ClientboundConfigurationFinishPacket().Call(context, null); 
     }
 }
