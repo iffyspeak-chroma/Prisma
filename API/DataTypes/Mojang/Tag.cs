f3eb9e99d0af848de73a7fefef2d1def55d1018d
Sonny <60908891+iffyspeak-chroma@users.noreply.github.com>
2026-03-04 04:53:34 -0600

    new tag packets

---
+++ API/DataTypes/Mojang/Tag.cs
@@ -0,0 +1,26 @@
+﻿using API.Networking;
+
+namespace API.DataTypes.Mojang;
+
+public class Tag : IWriteToPackets
+{
+    public Identifier TagName { get; private set; }
+    public List<int> Entries { get; private set; }
+
+    public Tag(Identifier name, List<int> entries)
+    {
+        this.TagName = name;
+        this.Entries = entries;
+    }
+    
+    public void Write(Packet packet)
+    {
+        TagName.Write(packet);
+        
+        packet.Write(Entries.Count);
+        foreach (int entry in Entries)
+        {
+            packet.Write(entry);
+        }
+    }
+}
---
+++ Server/Packets/Configuration/Clientbound/ClientboundConfigurationUpdateTagsPacket.cs
@@ -0,0 +1,58 @@
+﻿using API.DataTypes.Mojang;
+using API.Logging;
+using API.Networking;
+using DotNetty.Transport.Channels;
+using Server.Managers;
+
+namespace Server.Packets.Configuration.Clientbound;
+
+public class ClientboundConfigurationUpdateTagsPacket : ICallable
+{
+    public async void Call(IChannelHandlerContext context, Packet? packet)
+    {
+        Identifier registry = new Identifier("minecraft", "timeline");
+        List<Tag> tags = new List<Tag>()
+        {
+            new Tag(Identifier.Parse("minecraft:in_end"),
+                new List<int>()
+                {
+                    0
+                }),
+
+            new Tag(Identifier.Parse("minecraft:in_nether"),
+                new List<int>()
+                {
+                    0
+                }),
+
+            new Tag(Identifier.Parse("minecraft:in_overworld"),
+                new List<int>()
+                {
+                    0, 1, 2, 3
+                })
+        };
+
+        using (Packet p = new Packet())
+        {
+            // Only writing to 1 registry? (Some kind of something I didn't notice in the docs)
+            p.Write(1);
+            
+            registry.Write(p);
+
+            p.Write(tags.Count);
+            foreach (Tag tag in tags)
+            {
+                tag.Write(p);
+            }
+            
+            p.InsertInt(PacketReport.Mapping.Configuration.Clientbound["minecraft:update_tags"].Id);
+            p.WriteLength();
+
+            await PlayerManager.Instance.ConnectedClients[context.Channel].SendPacket(p);
+
+            LogTool.Debug(BitConverter.ToString(p.ToArray()).Replace("-", ""), true);
+        }
+        
+        new ClientboundConfigurationFinishPacket().Call(context, null);
+    }
+}
