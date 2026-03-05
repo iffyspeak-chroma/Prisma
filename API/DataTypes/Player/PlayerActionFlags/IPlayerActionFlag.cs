84a9ea82f343abea10a12f296cf963b2a353ba8f
Sonny <60908891+iffyspeak-chroma@users.noreply.github.com>
2026-03-04 21:42:33 -0600

    PlayerInfoUpdatePacket added.

---
+++ API/DataTypes/Player/PlayerActionFlags/IPlayerActionFlag.cs
@@ -0,0 +1,9 @@
+﻿using API.Networking;
+
+namespace API.DataTypes.Player.PlayerActionFlags;
+
+public interface IPlayerActionFlag
+{
+    byte GetMask();
+    Packet GetData();
+}
---
+++ Server/Packets/Play/Clientbound/ClientboundPlayPlayerInfoUpdatePacket.cs
@@ -0,0 +1,58 @@
+﻿using API.DataTypes.Player;
+using API.DataTypes.Player.PlayerActionFlags;
+using API.Networking;
+using DotNetty.Transport.Channels;
+using Server.Managers;
+
+namespace Server.Packets.Play.Clientbound;
+
+public class ClientboundPlayPlayerInfoUpdatePacket : ICallable
+{
+    public Dictionary<NetworkedClient, List<IPlayerActionFlag>> AffectedPlayers = new();
+        
+    public async void Call(IChannelHandlerContext context, Packet? packet)
+    {
+        NetworkedClient client = PlayerManager.Instance.ConnectedClients[context.Channel];
+        
+        using (Packet p = new Packet())
+        {
+            byte actionByte = 0;
+
+            foreach (KeyValuePair<NetworkedClient, List<IPlayerActionFlag>> kvp in AffectedPlayers)
+            {
+                List<IPlayerActionFlag> flags = kvp.Value;
+                
+                foreach (IPlayerActionFlag flag in flags)
+                {
+                    actionByte |= flag.GetMask();
+                }
+            }
+            
+            // Actions field
+            p.Write(actionByte);
+            
+            // Player count
+            p.Write(AffectedPlayers.Count);
+
+            //Player actions
+            foreach (KeyValuePair<NetworkedClient, List<IPlayerActionFlag>> kvp in AffectedPlayers)
+            {
+                NetworkedClient kvClient = kvp.Key;
+                List<IPlayerActionFlag> flags = kvp.Value;
+
+                ServerPlayer player = kvClient.Player;
+                
+                p.Write(player.Uuid);
+                foreach (IPlayerActionFlag flag in flags)
+                {
+                    p.Write(flag.GetData().ToArray());
+                }
+            }
+            
+            p.InsertInt(PacketReport.Mapping.Play.Clientbound["minecraft:player_info_update"].Id);
+            p.WriteLength();
+
+            await client.SendPacket(p);
+        }
+    }
+}
