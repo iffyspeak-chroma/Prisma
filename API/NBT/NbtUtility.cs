28e0f637f71b1b287f14884949de484241a258a6
Sonny <60908891+iffyspeak-chroma@users.noreply.github.com>
2026-03-15 16:57:29 -0500

    Create a way to get a compound's bytes

---
+++ API/NBT/NbtUtility.cs
@@ -0,0 +1,15 @@
+﻿using fNbt;
+
+namespace API.NBT;
+
+public class NbtUtility
+{
+    public static byte[] ConvertToBytes(NbtCompound compound)
+    {
+        var file = new NbtFile(compound);
+        using var stream = new MemoryStream();
+        file.SaveToStream(stream, NbtCompression.None);
+
+        return stream.ToArray();
+    }
+}
