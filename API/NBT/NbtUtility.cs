e60a60f81c0480085cc60be13bb781e508d05e22
Sonny <60908891+iffyspeak-chroma@users.noreply.github.com>
2026-03-15 17:16:21 -0500

    Since network nbt doesnt end with a compound end tag, try removing it.

--- API/NBT/NbtUtility.cs
+++ API/NBT/NbtUtility.cs
@@ -7,9 +7,18 @@ public class NbtUtility
     public static byte[] ConvertToBytes(NbtCompound compound)
     {
         var file = new NbtFile(compound);
+
         using var stream = new MemoryStream();
         file.SaveToStream(stream, NbtCompression.None);
 
-        return stream.ToArray();
+        var data = stream.ToArray();
+
+        // Remove the extra TAG_End written by NbtFile
+        if (data.Length > 0 && data[^1] == 0x00)
+        {
+            Array.Resize(ref data, data.Length - 1);
+        }
+
+        return data;
     }
 }
