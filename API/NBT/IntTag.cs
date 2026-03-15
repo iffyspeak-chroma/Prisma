7ab66fbef333344fa6d282911e2c78d299110c92
Sonny <60908891+iffyspeak-chroma@users.noreply.github.com>
2026-03-15 18:32:45 -0500

    Add IntTag

---
+++ API/NBT/IntTag.cs
@@ -0,0 +1,16 @@
+﻿namespace API.NBT;
+
+public class IntTag : AbstractTagType
+{
+    public override byte TagType => 3;
+
+    public IntTag(string name, int value)
+    {
+        TagName = name;
+        ValidateName();
+        
+        AddIdentifiers();
+
+        Payload.AddRange(BitConverter.GetBytes(value));
+    }
+}
