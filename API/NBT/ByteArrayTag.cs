edf7311a0d7159c0df4e7ebf4bd43bd62747c1ca
Sonny <60908891+iffyspeak-chroma@users.noreply.github.com>
2026-03-15 18:41:50 -0500

    Added ByteArrayTag

---
+++ API/NBT/ByteArrayTag.cs
@@ -0,0 +1,32 @@
+﻿namespace API.NBT;
+
+public class ByteArrayTag : AbstractTagType
+{
+    public override byte TagType => 7;
+
+    public ByteArrayTag(string name, byte[] value)
+    {
+        TagName = name;
+        ValidateName();
+        
+        AddIdentifiers();
+        
+        AddToPayload(value);
+    }
+
+    public ByteArrayTag(string name, List<byte> value)
+    {
+        TagName = name;
+        ValidateName();
+        
+        AddIdentifiers();
+        
+        AddToPayload(value.ToArray());
+    }
+
+    private void AddToPayload(byte[] v)
+    {
+        Payload.AddRange(BitConverter.GetBytes(v.Length));
+        Payload.AddRange(v);
+    }
+}
