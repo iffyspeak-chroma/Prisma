c503d7ac94d3f79a3cb877b3a0f45d37997b58e2
Sonny <60908891+iffyspeak-chroma@users.noreply.github.com>
2026-03-15 17:34:15 -0500

    Drop fNBT from our project.

--- API/TextComponents/TextComponentBuilder.cs
+++ API/TextComponents/TextComponentBuilder.cs
@@ -1,6 +1,5 @@
 ﻿using System.Text.Json;
 using System.Text.Json.Serialization;
-using fNbt;
 
 namespace API.TextComponents;
 
@@ -60,78 +59,6 @@ public class TextComponentBuilder
     public string ToJson()
     {
         return JsonSerializer.Serialize(ToObject(), new JsonSerializerOptions { WriteIndented = true });
-    }
-
-    private NbtCompound ConvertDictionary(string name, Dictionary<string, object> dict)
-    {
-        var compound = new NbtCompound(name);
-
-        foreach (var kv in dict)
-        {
-            switch (kv.Value)
-            {
-                case string s:
-                    compound.Add(new NbtString(kv.Key, s));
-                    break;
-
-                case bool b:
-                    compound.Add(new NbtByte(kv.Key, (byte)(b ? 1 : 0)));
-                    break;
-
-                case Dictionary<string, object> inner:
-                    compound.Add(ConvertDictionary(kv.Key, inner));
-                    break;
-            }
-        }
-
-        return compound;
-    }
-
-    /// <summary>
-    /// Builds the NbtCompound manually.
-    /// </summary>
-    public NbtCompound ToNbt()
-    {
-        var root = new NbtCompound("");
-        
-        root.Add(new NbtString("text", ""));
-        
-        var extraList = new NbtList("extra", NbtTagType.Compound);
-
-        foreach (var comp in _components)
-        {
-            var compound = new NbtCompound();
-
-            foreach (var kv in comp)
-            {
-                switch (kv.Value)
-                {
-                    case string s:
-                    {
-                        compound.Add(new NbtString(kv.Key, s));
-                        break;
-                    }
-
-                    case bool b:
-                    {
-                        compound.Add(new NbtByte(kv.Key, (byte)(b ? 1 : 0)));
-                        break;
-                    }
-
-                    case Dictionary<string, object> dict:
-                    {
-                        compound.Add(ConvertDictionary(kv.Key, dict));
-                        break;
-                    }
-                }
-            }
-
-            extraList.Add(compound);
-        }
-        
-        root.Add(extraList);
-
-        return root;
     }
 
     /// <summary>
