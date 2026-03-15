82068153c506bbdcdb8d0a1de6292da1c983d05e
Sonny <60908891+iffyspeak-chroma@users.noreply.github.com>
2026-03-15 18:25:20 -0500

    Add ShortTag

---
+++ API/NBT/ShortTag.cs
@@ -0,0 +1,19 @@
+﻿namespace API.NBT;
+
+// It's like tag but for short people
+public class ShortTag : AbstractTagType
+{
+    public override byte TagType => 2;
+    public override string? TagName { get; }
+    public override List<byte> Payload { get; set; }
+
+    public ShortTag(string name, short value)
+    {
+        TagName = name;
+        ValidateName();
+        
+        AddIdentifiers();
+        
+        Payload.AddRange(BitConverter.GetBytes(value));
+    }
+}
