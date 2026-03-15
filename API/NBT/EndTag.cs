5aa97f607acf93b5dafc31a44d37f1defd6262b8
Sonny <60908891+iffyspeak-chroma@users.noreply.github.com>
2026-03-15 18:07:58 -0500

    Have the end tag just to have all types of tags.

---
+++ API/NBT/EndTag.cs
@@ -0,0 +1,16 @@
+﻿using API.Logging;
+
+namespace API.NBT;
+
+public class EndTag : AbstractTagType
+{
+    public override byte TagType => 0;
+    public override string? TagName => null;
+    public override List<byte> Payload { get; set; }
+
+    public EndTag()
+    {
+        // Have this tag for completion’s sake
+        return;
+    }
+}
