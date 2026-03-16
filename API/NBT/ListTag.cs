2a602eb4045d41dc74f31fdd28d3f86dc85dc81e
Sonny <60908891+iffyspeak-chroma@users.noreply.github.com>
2026-03-15 19:00:45 -0500

    Add ListTag

---
+++ API/NBT/ListTag.cs
@@ -0,0 +1,30 @@
+﻿namespace API.NBT;
+
+public class ListTag<TType> : AbstractTagType where TType : AbstractTagType
+{
+    public override byte TagType => 9;
+
+    public ListTag(string name, List<TType> tags)
+    {
+        TagName = name;
+        ValidateName();
+        
+        AddIdentifiers();
+
+        if (tags.Count <= 0)
+        {
+            // Use the EndTag
+            Payload.Add(new EndTag().TagType);
+            return;
+        }
+        
+        Payload.Add(tags[0].TagType);
+        
+        Payload.AddRange(BitConverter.GetBytes(tags.Count));
+        foreach (var tag in tags)
+        {
+            Payload.AddRange(tag.Payload);
+        }
+    }
+    
+}
