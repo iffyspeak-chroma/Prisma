0f9fbde3060132632ff34b73397768f1437ed738
Sonny <60908891+iffyspeak-chroma@users.noreply.github.com>
2026-03-15 19:41:06 -0500

    Only "assemble" a compound tag when instructed.

--- API/NBT/CompoundTag.cs
+++ API/NBT/CompoundTag.cs
@@ -4,20 +4,25 @@ public class CompoundTag : AbstractTagType
 {
     public override byte TagType => 10;
     public List<AbstractTagType> Children = new();
+    public bool IsRoot;
 
-    public CompoundTag(string? name, bool root)
+    public CompoundTag(string? name, bool isRoot)
     {
         TagName = name;
+        IsRoot = isRoot;
         ValidateName();
 
         AddIdentifiers(true);
+    }
 
+    public void AssemblePayload()
+    {
         foreach (AbstractTagType child in Children)
         {
             Payload.AddRange(child.Payload);
         }
 
-        if (!root)
+        if (!IsRoot)
         {
             Payload.Add(new EndTag().TagType);
         }
