26949fc0903d5a7fe322692f4317df964d5c9671
Sonny <60908891+iffyspeak-chroma@users.noreply.github.com>
2026-03-15 19:19:47 -0500

    Add CompoundTag

---
+++ API/NBT/CompoundTag.cs
@@ -0,0 +1,25 @@
+﻿namespace API.NBT;
+
+public class CompoundTag : AbstractTagType
+{
+    public override byte TagType => 10;
+    public List<AbstractTagType> Children = new();
+
+    public CompoundTag(string? name, bool root)
+    {
+        TagName = name;
+        ValidateName();
+
+        AddIdentifiers(true);
+
+        foreach (AbstractTagType child in Children)
+        {
+            Payload.AddRange(child.Payload);
+        }
+
+        if (!root)
+        {
+            Payload.Add(new EndTag().TagType);
+        }
+    }
+}
