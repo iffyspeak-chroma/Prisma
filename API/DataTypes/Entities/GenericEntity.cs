764a73b4d837beb68536924dd09ca4866d73f78e
Sonny <60908891+iffyspeak-chroma@users.noreply.github.com>
2026-03-22 13:35:44 -0500

    Add a field for entities to have a unique ID.

--- API/DataTypes/Entities/GenericEntity.cs
+++ API/DataTypes/Entities/GenericEntity.cs
@@ -6,10 +6,21 @@ public class GenericEntity
     public Location Location;
     public Location Velocity;
     public Heading LookDirection;
+    public Guid Uid;
 
     public GenericEntity()
     {
         Location = new Location();
         Velocity = new Location();
+        LookDirection = new Heading();
+        Uid = Guid.NewGuid();
+    }
+    
+    public GenericEntity(Guid uuid)
+    {
+        Location = new Location();
+        Velocity = new Location();
+        LookDirection = new Heading();
+        Uid = uuid;
     }
 }
