2f1ab302a0329849177eed53f827a34f30e4254a
Sonny <60908891+iffyspeak-chroma@users.noreply.github.com>
2026-03-21 22:04:19 -0500

    Add some stuff for loading worlds.

---
+++ API/World/Dimension.cs
@@ -0,0 +1,13 @@
+﻿using API.DataTypes.Mojang;
+
+namespace API.World;
+
+public class Dimension
+{
+    public Identifier Identifier { get; private set; }
+
+    public Dimension(Identifier id)
+    {
+        Identifier = id;
+    }
+}
---
+++ API/World/World.cs
@@ -0,0 +1,30 @@
+﻿using API.DataTypes;
+using API.DataTypes.Mojang;
+
+namespace API.World;
+
+public class World
+{
+    public Identifier Identifier { get; private set; }
+    public Dimension Dimension { get; private set; }
+    public Location Spawn { get; private set; }
+
+    public World(Identifier id, Dimension dim, Location loc)
+    {
+        Identifier = id;
+        Dimension = dim;
+        Spawn = loc;
+    }
+
+    public World(Identifier id, Dimension dim)
+    {
+        Identifier = id;
+        Dimension = dim;
+        Spawn = new Location(0, 64, 0);
+    }
+
+    public void SetWorldSpawn(Location location)
+    {
+        Spawn = location;
+    }
+}
