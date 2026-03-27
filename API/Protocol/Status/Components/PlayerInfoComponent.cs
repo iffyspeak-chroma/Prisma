46bb25430a4a3ef12129490dcd268fe35f8d068a
Sonny <60908891+iffyspeak-chroma@users.noreply.github.com>
2026-03-27 17:08:17 -0500

    Have a default value for the PlayerName and UUID.

--- API/Protocol/Status/Components/PlayerInfoComponent.cs
+++ API/Protocol/Status/Components/PlayerInfoComponent.cs
@@ -4,9 +4,7 @@ namespace API.Protocol.Status.Components;
 
 public class PlayerInfoComponent
 {
-    [JsonPropertyName("name")]
-    public string PlayerName { get; set; }
+    [JsonPropertyName("name")] public string PlayerName { get; set; } = "null";
         
-    [JsonPropertyName("id")]
-    public string UUID { get; set; }
+    [JsonPropertyName("id")] public string UUID { get; set; } = "00000000-0000-0000-0000-000000000000";
 }
