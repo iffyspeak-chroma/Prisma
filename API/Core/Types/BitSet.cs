a1ef62757a5174eaa4718029ebbadd42dd586837
Sonny <60908891+iffyspeak-chroma@users.noreply.github.com>
2026-04-04 18:58:39 -0500

    Add BitSet class

---
+++ API/Core/Types/BitSet.cs
@@ -0,0 +1,26 @@
+﻿namespace API.Core.Types;
+
+public class BitSet
+{
+    private readonly List<long> data = new();
+
+    public void Set(int index)
+    {
+        int longIndex = index / 64;
+
+        while (data.Count <= longIndex)
+        {
+            data.Add(0);
+        }
+
+        data[longIndex] |= 1L << (index % 64);
+    }
+
+    public bool Get(int index)
+    {
+        int longIndex = index / 64;
+        if (longIndex >= data.Count) return false;
+
+        return (data[longIndex] & (1L << (index % 64))) != 0;
+    }
+}
