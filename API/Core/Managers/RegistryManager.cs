69f8ffd6b52065b316f740774c17c7cf42bb6d68
Sonny <60908891+iffyspeak-chroma@users.noreply.github.com>
2026-04-04 19:36:57 -0500

    Remove unecessary function

--- API/Core/Managers/RegistryManager.cs
+++ API/Core/Managers/RegistryManager.cs
@@ -8,12 +8,4 @@ public class RegistryManager
 
     public List<Registry> Registries { get; } = new();
     public List<TagEntry> Tags { get; } = new();
-    
-    public static HashSet<string> FlatRegistries = new HashSet<string>
-    {
-        "damage_type",
-        "timeline",
-        "world_clock",
-        "dialog"
-    };
 }
