865dcb05da81061d9a81ff4d8b630db92b2b29eb
Sonny <60908891+iffyspeak-chroma@users.noreply.github.com>
2026-04-04 20:24:28 -0500

    Add some percentage functions to ExtendedMath

--- API/Core/ExtendedMath.cs
+++ API/Core/ExtendedMath.cs
@@ -9,4 +9,20 @@ public class ExtendedMath
         if (x <= 1) return 0;
         return BitOperations.Log2((uint)(x - 1)) + 1;
     }
+
+    public static float Percentage(int min, int max, int t)
+    {
+        if (max == min) return 0f;
+
+        float pct = (t - min) / (float)(max - min);
+
+        return Math.Clamp(pct, 0f, 1f);
+    }
+
+    public static float Scale(float rMin, float rMax, float rT, float sMin, float sMax)
+    {
+        float pct = (rT - rMin) / (rMax - rMin);
+        
+        return sMin + pct * (sMax - sMin);
+    }
 }
